dfb119f4e184864d3b277afc48478f7e0b91d175
agrahafiz13 <agrahafiz2@gmail.com>
2026-04-12 13:52:15 +0700

    feat: Core/Router
    membuat file app_router untuk meroute page]

---
+++ lib/core/route/app_router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:toku_store/core/constants/api_colors.dart';
+import 'package:toku_store/main.dart';
+import 'package:toku_store/features/auth/presentation/pages/dashboard_page.dart';
+import 'package:toku_store/features/auth/presentation/pages/login_page.dart';
+import 'package:toku_store/features/auth/presentation/pages/register_page.dart';
+import 'package:toku_store/features/auth/presentation/pages/verify_email_page.dart';
+import 'package:toku_store/features/splash/presentation/pages/splash_page.dart';
+
+class AppRouter {
+  static const String splash = '/';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String dashboard = '/dashboard';
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash: (_) => const SplashPage(),
+    login: (_) => const LoginPage(),
+    register: (_) => const RegisterPage(),
+    verifyEmail: (_) => const VerifyEmailPage(),
+    dashboard: (_) => const AuthGuard(child: DashboardPage()),
+  };
+}
