6b4d3387e544edcb59775151e1f7d60ff0f3834e
agrahafiz13 <agrahafiz2@gmail.com>
2026-04-12 15:12:45 +0700

    feat: Dashboard/Domain
    membuat file auth_repository_impl

---
+++ lib/features/dashboard/domain/repositories/auth_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:toku_store/core/constants/api_constants.dart';
+import 'package:toku_store/core/services/dio_client.dart';
+import 'package:toku_store/features/dashboard/domain/repositories/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  @override
+  Future<String> verifyFirebaseToken(String firebaseToken) async {
+    final response = await DioClient.instance.post(
+      ApiConstants.verifyToken,
+      data: {'firebase_token': firebaseToken},
+    );
+
+    final data = response.data['data'] as Map<String, dynamic>;
+    return data['access_token'] as String;
+  }
+}
